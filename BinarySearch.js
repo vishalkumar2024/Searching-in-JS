f7c881f74e3f58bfe7926117f549313c5d581f99
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-07-07 13:37:54 +0530

    correction in binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -3,19 +3,21 @@
 function BinarySearch(arr, k) {
     let start = 0;
     let end = arr.length - 1;
+    let ans=-1;
 
     while (start <= end) {
         let mid = Math.floor((start + end) / 2);
 
         if (arr[mid] === k) {
-            return mid; // Found target, return index
+            ans=mid;
+            end=mid-1 
         } else if (k > arr[mid]) {
             start = mid + 1; // Search right half
         } else {
             end = mid - 1; // Search left half
         }
     }
-    return -1
+    return ans
 }
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(BinarySearch(arr, 4));
