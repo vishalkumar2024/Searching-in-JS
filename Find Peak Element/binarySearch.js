a3a4126ddf818f69efad888a29b7108b799276fe
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-07-10 17:13:34 +0530

    added optimal solution for find peak elemnnt folder

---
+++ Find Peak Element/binarySearch.js
@@ -0,0 +1,30 @@
+// Leetcode 162 - but only if there is only one peak element
+
+function findPeakElement(arr) {
+    if (arr.length == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[arr.length - 1] > arr[arr.length - 2]) return arr.length - 1
+
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (arr[mid - 1] < arr[mid] && arr[mid + 1] < arr[mid]) return mid;
+
+        if (arr[mid] > arr[mid - 1]) {
+            start = mid + 1;
+        } else if (arr[mid] > arr[mid + 1]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1
+        }
+    }
+}
+
+let arr = [1, 2, 1, 3, 5, 6, 4];
+console.log(findPeakElement(arr));
+
+// TC=O(log(n))
+// SC=O(1)
